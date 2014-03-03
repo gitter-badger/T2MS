893063d80484e54936e2de6ba4f0cd2898d74bab
madawa-rc <madawa.11@cse.mrt.ac.lk>
2014-03-03 15:57:09 +0530

    Merge

--- app/Controller/SmsController.php
+++ app/Controller/SmsController.php
@@ -5,15 +5,19 @@ App::uses('AppController', 'Controller');
 
 class SmsController extends AppController{
 
-	
+
 	public function index() {
 		$this->loadModel('Customer');
 		if($this->request->is('get')&&$this->request->query!=null){
 			$this->Customer->create();
-			$data = array("Customer"=>array("phone" => $this->request->query['phone'], "name" => $this->request->query['text'],
-			"blacklisted"=>0,"maxFare" => 60));
+			/*$data = array("Customer"=>array("phone" => $this->request->data['Sms']['phone'], "name" => $this->request->data['Sms']['text'],
+			"blacklisted"=>0,"maxFare" => 60));*/
+            $message = $this->request->data['Sms']['text'];
+            $phone = $this->request->data['Sms']['phone'];
 
-			if ($this->Customer->save($data)) {
+            $this->decodeSms($message,$phone);
+
+			/*if ($this->Customer->save($data)) {
 				
 				$this->Session->setFlash(__('The SMS has been processed.'));
 				$this->request->query=null;
@@ -22,8 +26,13 @@ class SmsController extends AppController{
 				$this->Session->setFlash(__( 'The SMS could not be processed. Please, try again.'));
 			}
 			//echo(json_encode($this->request));
+			}*/
 		}
 	}
+
+    private function decodeSms($message, $phone){
+        echo($message);
+    }
 
 }
 
